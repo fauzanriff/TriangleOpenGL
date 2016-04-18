f5821a7478bb6adf056545a4cdd4f327831f4225
Wiwit Rifa'i <wiwitrifai@gmail.com>
2016-04-18 15:52:57 +0700

    Rubik done

---
+++ include/test/gtc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Projects/TriangleOpenGL/include/glm/test/gtc
+# Build directory: D:/Projects/TriangleOpenGL/include/test/gtc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ include/test/gtx/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Projects/TriangleOpenGL/include/glm/test/gtx
+# Build directory: D:/Projects/TriangleOpenGL/include/test/gtx
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
